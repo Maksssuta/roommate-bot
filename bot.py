069c1bc204253462252361a1af4a1de3492549da
Maksssuta <Maksssuta764@yandex.ru>
2025-08-27 16:53:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from collections import defaultdict
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-from aiogram.filters import Command
 from aiogram.fsm.filters import StateFilter
 from aiogram.fsm.storage.memory import MemoryStorage
 
